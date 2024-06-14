0c6e50dc1cb9753b3e90780e6312119324845978
Imamuddin Shaik <85993981+Shaik-36@users.noreply.github.com>
2024-06-14 23:23:27 +0100

    Added Models

---
+++ Data/Student_MVCDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Student_MVC.Models
+{
+    public class Student_MVCDBContext : DbContext
+    {
+        public Student_MVCDBContext (DbContextOptions<Student_MVCDBContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Student_MVC.Models.Student> Student { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_MVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public int SId { get; set; }
+
+        public string StudentName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Course { get; set; }
+
+        public int Fee{ get; set; }
+    }
+}
